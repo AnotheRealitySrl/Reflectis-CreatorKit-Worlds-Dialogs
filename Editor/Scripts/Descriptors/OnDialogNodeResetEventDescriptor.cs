df4d37a751c45c065f60146a8280a7b60928d46e
Elia Russo <rockrono@gmail.com>
2025-03-20 19:24:08 +0100

    New event node (Dialog Part): OnDialogNodeReset

--- Editor/Scripts/Descriptors/OnDialogNodeResetEventDescriptor.cs
+++ Editor/Scripts/Descriptors/OnDialogNodeResetEventDescriptor.cs
@@ -1,19 +1,22 @@
-using UnityEngine;
+using Unity.VisualScripting;
 
 namespace Reflectis.CreatorKit.Worlds.Dialogs.Editor
 {
-    public class OnDialogNodeResetEventDescriptor : MonoBehaviour
+    [Descriptor(typeof(OnDialogNodeResetEventNode))]
+    public class OnDialogNodeResetEventDescriptor : EventUnitDescriptor<OnDialogNodeResetEventNode>
     {
-        // Start is called once before the first execution of Update after the MonoBehaviour is created
-        void Start()
+        public OnDialogNodeResetEventDescriptor(OnDialogNodeResetEventNode unit) : base(unit) { }
+
+        protected override string DefinedSummary()
         {
-        
+            return "This event will be triggered when the referenced Dialog Part gets " +
+                "its state reset to \"ToDo\" state. This happens whenever a dialogue " +
+                "sequence involving this dialong part/node gets completed or canceled.";
         }
 
-        // Update is called once per frame
-        void Update()
+        protected override void DefinedPort(IUnitPort port, UnitPortDescription description)
         {
-        
+            base.DefinedPort(port, description);
         }
     }
 }
--- Runtime/Scripts/Nodes/EventUnit/OnDialogNodeResetEventNode.cs
+++ Runtime/Scripts/Nodes/EventUnit/OnDialogNodeResetEventNode.cs
@@ -1,19 +1,60 @@
-using UnityEngine;
+using Reflectis.SDK.Dialogs;
+using Unity.VisualScripting;
 
 namespace Reflectis.CreatorKit.Worlds.Dialogs
 {
-    public class OnDialogNodeResetEventNode : MonoBehaviour
+    [UnitTitle("Reflectis Dialogs: On Dialog Node Reset")]
+    [UnitSurtitle("Dialogs")]
+    [UnitShortTitle("On Dialog Node Reset")]
+    [UnitCategory("Events\\Reflectis")]
+    public class OnDialogNodeResetEventNode : EventUnit<Null>
     {
-        // Start is called once before the first execution of Update after the MonoBehaviour is created
-        void Start()
+        public static string eventName = "DialogPathEnded";
+
+        protected override bool register => true;
+
+        protected GraphReference graphReference;
+
+        protected DialogPart dialogPartReference;
+
+        [DoNotSerialize]
+        [PortLabel("Dialog Part")]
+        public ValueInput DialogPartReference { get; private set; }
+
+        protected override void Definition()
         {
-        
+            base.Definition();
+            DialogPartReference = ValueInput<DialogSystem>(nameof(DialogPartReference));
         }
 
-        // Update is called once per frame
-        void Update()
+        public override void Instantiate(GraphReference instance)
         {
-        
+            base.Instantiate(instance);
+
+            using (var flow = Flow.New(instance))
+            {
+                dialogPartReference = flow.GetValue<DialogPart>(DialogPartReference);
+            }
+            dialogPartReference.OnDialogReset.AddListener(OnDialogNodeReset);
+        }
+
+        public override EventHook GetHook(GraphReference reference)
+        {
+            graphReference = reference;
+
+            return new EventHook(eventName);
+        }
+
+        public override void Uninstantiate(GraphReference instance)
+        {
+            base.Uninstantiate(instance);
+
+            dialogPartReference.OnDialogReset.RemoveListener(OnDialogNodeReset);
+        }
+
+        private void OnDialogNodeReset()
+        {
+            Trigger(graphReference, null);
         }
     }
 }
